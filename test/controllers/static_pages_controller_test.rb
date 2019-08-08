13bc2b13658ec23d89fea29c8455b9f47a325798
Brian Gwaltney <brian.gwaltney@gmail.com>
2019-08-08 13:25:39 +0700

    updated layout and routing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,14 +13,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', "About | #{@base_url}"
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_url}"
+  end
+
+  test 'should get contact' do
+    get contact_path
+    assert_response :success
+    assert_select 'title', "Contact Us | #{@base_url}"
   end
 end
