83f493e941a7d5d3ee8685bc00cf9c2ff8a337ab
Brian Gwaltney <brian.gwaltney@gmail.com>
2019-08-11 00:58:43 +0700

    updated database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2019_08_10_125517) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", id: :serial, force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
@@ -47,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_08_10_125517) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
